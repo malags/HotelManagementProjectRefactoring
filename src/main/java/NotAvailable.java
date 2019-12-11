c6daf7c07ee05d8845258a88a7b27f150aa19f0e
Stefano Malago <malags3333@gmail.com>
2019-12-11 16:19:40 +0100

    class invariant for NotAvailable

--- src/main/java/NotAvailable.java
+++ src/main/java/NotAvailable.java
@@ -7,6 +7,12 @@ class NotAvailable extends Exception implements Contract {
 
     protected int id;
 
+    @Invariant
+    @Pure
+    protected boolean room_has_valid_number(){
+        return this.id >= 1 && this.id <= 60;
+    }
+
     public NotAvailable(int id) {
         this.id = id;
     }
